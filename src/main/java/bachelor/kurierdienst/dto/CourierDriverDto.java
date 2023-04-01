5787e1f44fc641378247f73c93db0f27a883328c
kirill.esau <kirill.esau@adesso.de>
2023-04-01 21:57:03 +0200

    Boilerplate Code aus den DTO's entfernt

--- src/main/java/bachelor/kurierdienst/dto/CourierDriverDto.java
+++ src/main/java/bachelor/kurierdienst/dto/CourierDriverDto.java
@@ -1,50 +1,13 @@
 package bachelor.kurierdienst.dto;
 
+import lombok.Value;
+
+@Value
 public class CourierDriverDto {
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
 
-    public CourierDriverDto() {}
-
-    public CourierDriverDto(String firstName, String lastName, String email, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
